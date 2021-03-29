4d23bb9675d2a582bca92159c9de329e0b4767c1
nicolasCaballero <niicolas.caballero@gmail.com>
2021-03-29 20:40:15 -0300

    Clase 5

---
+++ clase5/ejercicios/diapositiva28.js
@@ -0,0 +1,18 @@
+let nums = new Array(1000);
+
+let generateRandomNumbers = () => {
+    let i;
+    for (let i = 0; i < nums.length; i++) {
+        nums[i] = Math.floor((Math.random() * 20) + 1);
+    };
+};
+
+generateRandomNumbers()
+
+var objFromArrs = {};
+
+nums.forEach(function(numbers){
+    objFromArrs[numbers] = (objFromArrs[numbers] || 0) + 1;
+});
+
+console.log(objFromArrs);
