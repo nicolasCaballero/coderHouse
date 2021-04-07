221d542755373a18a5379e07cb3512d6db5fb248
nicolasCaballero <niicolas.caballero@gmail.com>
2021-04-07 20:28:23 -0300

    update ejercicios clase7

--- clase7 - NPM e introducción a express/ejercicios/diapositiva39/index.js
+++ clase7 - NPM e introducción a express/ejercicios/diapositiva39/index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import fs from 'fs';
+
 const app = express();
 const port = 8080;
 const server = app.listen(port, () => {
@@ -6,12 +8,36 @@ const server = app.listen(port, () => {
 });
 server.on(`error`, error => console.log(`Server error ${error}`));
 
+/* EJERCICIO A) */
 app.get('/', (req, res) => {
-    res.json({msg: `Hey!`});
+    res.send('<h1>Welcome to the Express Server!</h1> <style>h1{color:blue}</style>');
 });
 
-let date = Date.now();
-app.get('/fyh', (req, res) => {
-    res.json(date)
+/* EJERCICIO B) */
+app.get('/visitas', (req, res) => {
+    let file = fs.readFile('./visitas.txt', 'utf-8', (err, cont) => {
+
+        if (err) {
+
+            let newFile = [{
+                "times": 1
+            }];
+
+            res.send(`You're the first person ever to visit this page! Congrats!`);
+            fs.writeFileSync('./visitas.txt', JSON.stringify(newFile, null, 1));
+        } else {
+
+            let parsedJSON = JSON.parse(cont);
+            let timesVisited = parsedJSON[0].times + 1;
+            res.send(`This page had been visited ${timesVisited} times!`);
+            parsedJSON[0].times = parsedJSON[0].times + 1;
+            fs.writeFileSync('./visitas.txt', JSON.stringify(parsedJSON, null, 1));
+        };
+    });
 });
-/* 11/1/2021 11:36:04 */
+
+/* EJERCICIO C) */
+app.get('/fyh', (req, res) => {
+    let date = new Date();
+    res.send(`${date.getDate()}/${date.getMonth()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`)
+});
